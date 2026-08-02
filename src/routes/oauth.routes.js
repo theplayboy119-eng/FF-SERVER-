a143e3aa3000e5474436ac466796d790168ae40f
theplayboy119-eng <theplayboy119@gmail.com>
2026-08-02 02:27:10 -0400

    Update oauth.routes.js

--- src/routes/oauth.routes.js
+++ src/routes/oauth.routes.js
@@ -4,19 +4,19 @@ const oauthController = require('../controllers/oauth.controller');
 const { verifyToken } = require('../middlewares/auth.middleware');
 
 // Création d'un compte invité (Guest Account)
-router.post('/guest/register', oauthController.registerGuest);
+router.post('/guest/register', oauthController.guestRegister);
 
 // Obtention d'un jeton d'accès pour un compte invité
-router.post('/guest/token/grant', oauthController.grantGuestToken);
+router.post('/guest/token/grant', oauthController.guestTokenGrant);
 
 // Authentification principale (Garena / Email / Mot de passe)
 router.post('/token', oauthController.loginToken);
 
 // Échange d'un jeton tiers (Facebook, Google, VK, Line)
-router.post('/token/exchange', oauthController.exchangeToken);
+router.post('/token/exchange', oauthController.tokenExchange);
 
 // Rafraîchissement d'un jeton d'accès expiré
-router.post('/token/refresh', oauthController.refreshToken);
+router.post('/token/refresh', oauthController.tokenRefresh);
 
 // Déconnexion de la session joueur
 router.post('/logout', verifyToken, oauthController.logout);
