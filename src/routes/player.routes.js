ab29c9350531d0f887e2023af973e7932ab129cc
theplayboy119-eng <theplayboy119@gmail.com>
2026-08-02 02:48:56 -0400

    Update player.routes.js

--- src/routes/player.routes.js
+++ src/routes/player.routes.js
@@ -1,4 +1,4 @@
-Const express = require('express');
+const express = require('express');
 const router = express.Router();
 const playerController = require('../controllers/player.controller');
 const { verifyToken } = require('../middlewares/auth.middleware');
